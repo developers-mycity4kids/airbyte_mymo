319b221554f02270fc5f92f4afa418ae55935774
developers-mycity4kids <developers@mycity4kids.com>
2022-08-22 16:18:56 +0530

    update table name

--- models/mymo/mymo_campaign-user_deliverable_details.sql
+++ models/mymo/mymo_campaign-user_deliverable_details.sql
@@ -10,4 +10,4 @@
 {{ config(materialized='table') }}
 
 SELECT id, is_deleted, created_at, updated_at, deliverable_type, brief, read_this, photo_instructions, caption, tags, hash_tags, other_rules, terms, example, campaign_id, product_purchase_link, product_review_link, survey_link, campaign_link, proof_allowed, proof_required, task_detail_id, sort_order, description, to_timestamp(start_date) as start_date, to_timestamp(end_date) as end_date, auto_rejection_days, auto_rejection_hours, step_instructions_media, script_instructions, meta_info, buy_now_link
-FROM public.airbyte_deliverable_details
+FROM public.mymo_campaign-user_deliverable_details
